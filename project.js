bc7b112f72c8517cd31555291a8888e5889e7fed
ohonen <oren.honen@gmail.com>
2014-03-10 10:34:30 +0200

    added semicolon to disable error marking

--- project.js
+++ project.js
@@ -1,11 +1,11 @@
 function showResult(str)
 {
 	if (str.length==0)
-	  {
-		  document.getElementById("livesearch").innerHTML="";
-		  document.getElementById("livesearch").style.border="0px";
-		  return;
-	  }
+	{
+		document.getElementById("livesearch").innerHTML="";
+		document.getElementById("livesearch").style.border="0px";
+		return;
+	}
 
 	if (window.XMLHttpRequest)
   	{// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -19,7 +19,7 @@ function showResult(str)
 	    	document.getElementById("livesearch").innerHTML=xmlhttp.responseText;
     		document.getElementById("livesearch").style.border="1px solid #A5ACB2";
     	}
-	}
+	};
 	
 xmlhttp.open("GET","livesearch.php?q="+str,true);
 xmlhttp.send();
